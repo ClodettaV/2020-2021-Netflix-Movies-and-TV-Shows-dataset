44c90fd9e4166684f9ca9c5a2a3592becc124c49
Daniel <daniel.f.bourke@gmail.com>
2022-01-20 22:08:56 +0800

    group work on Thursday, visuals coming along

--- app.py
+++ app.py
@@ -1,29 +1,224 @@
-from flask import Flask, render_template
-from sqlalchemy import create_engine
+from flask import Flask, session, request, redirect, render_template, Blueprint, jsonify
+from flask_restx import Api, Resource, fields
+from flask_restful import Api, Resource, fields
+import pandas as pd
+import numpy as np
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+import json
+import pickle
+import markdown as md
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 # database setup 
-engine = create_engine("sqlite:///Netflix.sqlite")
+
+# df = pd.read_csv("netflix_movies_tvshows.csv")
+
 
 # flask app setup
 app = Flask(__name__)
 
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Netflix_moviesandTvshows.sqlite"
+# db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+# NETFLIX=Base.prepare(engine, reflect=True)
+
+
+# class mytable(db.Model):
+#     __tablename__ = 'netflix'
+#     show_id = db.Column(db.String, primary_key=True)
+#     type = db.Column(db.String)
+#     title = db.Column(db.String)
+#     release_year = db.Column(db.Integer)
+    # quantity = db.Column(db.Integer)
+    # price = db.Column(db.Float)
+    # updated = db.Column(db.String)
+
+# NETFLIX = Base.classes.mytable
+
+
+# metricMapper = {
+#     "revenue": "Revenue",
+#     "cost": "Product cost",
+#     "profit": "Gross profit"
+# }
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    engine = create_engine("sqlite:///Netflix_moviesandTvshows.sqlite")
+    conn= engine.connect()
+    yearsdf = pd.read_sql("select distinct release_year from netflix ",conn)
+    years = yearsdf["release_year"].sort_values(ascending=False).to_list()
+    return render_template("index.html", years = years)
 
-@app.route("/api/names")
+@app.route("/director/<year>/<type>")
+def directorgroup(year,type):
+    engine = create_engine("sqlite:///Netflix_moviesandTvshows.sqlite")
+    conn= engine.connect()
+    sqldata = pd.read_sql("select * from netflix ",conn)
+    df = pd.DataFrame(sqldata)
+    grouped= df[(df["release_year"].isin(map(int,year.split(','))))&(df["type"].isin(type.split(',')))].groupby(["director"]).count().sort_values("title",ascending=False)["title"].reset_index()[0:10]
+    jsondata = grouped.to_json(orient="records")
+
+    return jsondata
+
+@app.route("/rating/<year>/<type>")
+def ratinggroup(year,type):
+    engine = create_engine("sqlite:///Netflix_moviesandTvshows.sqlite")
+    conn= engine.connect()
+    sqldata = pd.read_sql("select * from netflix ",conn)
+    df = pd.DataFrame(sqldata)
+    grouped= df[(df["release_year"].isin(map(int,year.split(','))))&(df["type"].isin(type.split(',')))].groupby(["rating"]).count().sort_values("title",ascending=False)["title"].reset_index()[0:10]
+    jsondata = grouped.to_json(orient="records")
+
+    return jsondata
+
+@app.route("/type/<year>/<type>")
+def typegroup(year,type):
+    engine = create_engine("sqlite:///Netflix_moviesandTvshows.sqlite")
+    conn= engine.connect()
+    sqldata = pd.read_sql("select * from netflix ",conn)
+    df = pd.DataFrame(sqldata)
+    grouped= df[(df["release_year"].isin(map(int,year.split(','))))&(df["type"].isin(type.split(',')))].groupby(["type"]).count().sort_values("title",ascending=False)["title"].reset_index()[0:10]
+    jsondata = grouped.to_json(orient="records")
+
+    return jsondata
+
+@app.route("/year/<year>/<type>")
+def yeargroup(year,type):
+    engine = create_engine("sqlite:///Netflix_moviesandTvshows.sqlite")
+    conn= engine.connect()
+    sqldata = pd.read_sql("select * from netflix ",conn)
+    df = pd.DataFrame(sqldata)
+    grouped= df[(df["release_year"].isin(map(int,year.split(','))))&(df["type"].isin(type.split(',')))].groupby(["release_year"]).count().sort_values("title",ascending=False)["title"].reset_index()[0:10]
+    jsondata = grouped.to_json(orient="records")
+
+    return jsondata
+
+# @app.route("/inventory/<release_year>")
+# def inventory(release_year):
+#         stuff = mytable.query.filter_by(release_year=release_year).order_by(mytable.show_id).all()
+#         sock_text = '<ul>'
+#         for sock in stuff:
+#             sock_text += '<li>' + sock.show_id + ', ' + str(sock.release_year) + '</li>'
+#         sock_text += '</ul>'
+#         return render_template("index.html",stuff=stuff, release_year=release_year )
+
+
+
+
+
+# @app.route("/NETFLIX/<release_year>")
+# def netflix(release_year):
+#     sel = [
+#         NETFLIX.show_id,
+#         NETFLIX.type,
+#         NETFLIX.title,
+#         NETFLIX.release_year,
+
+#     ]
+
+#     results = db.session.query(*sel).filter(NETFLIX.release_year == release_year).all()
+
+#     Netflix={}
+#     for result in results:
+#         Netflix["show_id"]=result[0]
+#         Netflix["type"]=result[1]
+#         Netflix["title"]=result[2]
+#         Netflix["release_year"]=result[3]
+#     print(Netflix)
+#     return jsonify(Netflix)
+
+
+@app.route("/api/release_year")
 def names():
     results = [
         {
-            "id": list(row)[0],
-            "name": list(row)[1],
-            "pclass": list(row)[2],
-            "age": list(row)[3],
-            "sex": list(row)[4],
-            "survived": list(row)[5],
-        } for row in engine.execute("select * from country").all()]
+            "show_id": list(row)[0],
+            "type": list(row)[1],
+            "title": list(row)[2],
+            "director": list(row)[3],
+            "cast": list(row)[4],
+            "country": list(row)[5],
+            "release_year": list(row)[7],
+            "rating": list(row)[8],
+            "listed_in": list(row)[9],
+            "description": list(row)[10]
+        
+        } for row in engine.execute("select * from netflix")]
 
-    return {"passengers":results}
+    return {"netflix":results}
+
+
+
+# # Route for map
+# @app.route("/map")
+# def map():
+#    return render_template("map.html")
+
+# @app.route("/titlesByDirector/<metric>/<years>")
+# class titlesByDirector(Resource):
+#     def get(self, metric, years):
+#         return df[df["release_year"].isin(map(int, years.split(",")))].groupby(by=["release_year"]).sum()[metricMapper[metric]].to_dict()
+
+# def chart():
+#    return render_template("chart.html")
+
+# @app.route("/samples/<show_id>")
+# def samples(show_id):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(netflix).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
+
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[show_id] > 1, ["otu_id", "otu_label", show_id]]
+#     sample_data.sort_values(show_id, axis=0, ascending=False, inplace=True)
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[show_id].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
+
+
+    
+# Route to get all the Electric Vehicle Charging Stations data.
+# Read data from Stations table in json format and return it to the browser.
+# @app.route("/stations")
+# def getAllStations():
+#     conn = engine.connect()
+#     query = f"select state,station_name, latitude, longitude, open_date from Stations"
+#     df = pd.read_sql(query, conn)
+#     stations = df.to_json(orient="records")
+    
+#     return stations
+#     #return render_template("stations.html")
+
+# @app.route("/emissions")
+# def getAllEmissions():
+#     conn = engine.connect()
+#     query = f"select * from netflix"
+#     df = pd.read_sql(query, conn)
+#     emissions = df.to_json(orient="records")
+    
+#     return {"netflix":emissions}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,157 +1,351 @@
-function buildMetadata(sample) {
-  d3.json("netflix_movies_tvShows.csv").then((data) => {
-      var metadata= data.metadata;
-      var resultsarray= metadata.filter(sampleobject => 
-        sampleobject.id == sample);
-      var result= resultsarray[0]
-      var panel = d3.select("#sample-metadata");
-      panel.html("");
-      Object.entries(result).forEach(([key, value]) => {
-        panel.append("h6").text(`${key}: ${value}`);
-      });
-  
-    });
-  }
-  
-  function buildCharts(sample) {
-  
-  // Using `d3.json` to fetch the sample data for the plots
-  d3.json("netflix_movies_tvshows.csv").then((data) => {
-    var samples= data.samples;
-    var resultsarray= samples.filter(sampleobject => 
-        sampleobject.id == sample);
-    var result= resultsarray[0]
-  
-    var ids = result.otu_ids;
-    var labels = result.otu_labels;
-    var values = result.sample_values;
+// var eyeColor = ["Brown", "Brown", "Brown", "Brown", "Brown",
+//   "Brown", "Brown", "Brown", "Green", "Green",
+//   "Green", "Green", "Green", "Blue", "Blue",
+//   "Blue", "Blue", "Blue", "Blue"];
+// var eyeFlicker = [26.8, 27.9, 23.7, 25, 26.3, 24.8,
+//   25.7, 24.5, 26.4, 24.2, 28, 26.9,
+//   29.1, 25.7, 27.2, 29.9, 28.5, 29.4, 28.3];
 
+// // Create the Trace
+// var trace1 = {
+//   x: eyeColor,
+//   y: eyeFlicker,
+//   type: "bar"
+// };
 
-    // Build a bubble Chart 
+// // Create the data array for the plot
+// var data = [trace1];
 
+// // Define the plot layout
+// var layout = {
+//   title: "Eye Color vs Flicker",
+//   xaxis: { title: "Eye Color" },
+//   yaxis: { title: "Flicker Frequency" }
+// };
 
-    var LayoutBubble = {
-      margin: { t: 0 },
-      xaxis: { title: "OTU ID" },
-      hovermode: "closest",
-      
-      };
-  
-      var DataBubble = [ 
-      {
-        x: ids,
-        y: values,
-        text: labels,
-        mode: "markers",
-        marker: {
-          color: ['rgb(135,206,235)', 'rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(245,222,179)','rgb(255,218,185)'], 
-          size: values,
-          },
-      }
-    ];
-  
-    Plotly.newPlot("bubble", DataBubble, LayoutBubble);
-  
-  
-    //Build a bar chart
+// // Plot the chart to a div tag with id "bar-plot"
+// Plotly.newPlot("moviesVsTV", data, layout);
 
-    var bar_data =[
-      {
-        y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
-        x:values.slice(0,10).reverse(),
-        text:labels.slice(0,10).reverse(),
-        type:"bar",
-        orientation:"h",
-        marker:{
-            color: 'rgb(135,206,235)',
-        }
-      }
-    ];
-  
-    var barLayout = {
-      title: "Top 10 Bacteria Cultures Found",
-      margin: { t: 30, l: 150 },
+function renderDirector(year, type) {
+  d3.json(`/director/${year}/${type}`).then(data2 => {
+    var trace1 = {
+      y: data2.map(row => row.director),
+      x: data2.map(row => row.title).reverse(),
+      type: "bar",
+      orientation: "h"
+
     };
-  
-    Plotly.newPlot("bar", bar_data, barLayout);
-  });
-  }
-   
-  
-  function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-  
-  // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-  
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-  }
-  
-  function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-  }
+    // Create the Trace
 
-  
-  // Initialize the dashboard
-  init();
 
-// {% extends 'layout.html' %} {% block content %}
-// <div>
-//   <h1>The Analysis of the files selected are :</h1>
-//   <canvas id="myChart" width="400" height="400"></canvas>
-//   <script>
-//     var ctx = document.getElementById("myChart").getContext("2d");
-//     var dataframe = "{{ dataframe }}";
-//     var myChart = new Chart(dataframe, {
-//       type: "bar",
-//       data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [
-//           {
-//             label: "# of Votes",
-//             data: dataframe,
-//             backgroundColor: [
-//               "rgba(255, 99, 132, 0.2)",
-//               "rgba(54, 162, 235, 0.2)",
-//               "rgba(255, 206, 86, 0.2)",
-//               "rgba(75, 192, 192, 0.2)",
-//               "rgba(153, 102, 255, 0.2)",
-//               "rgba(255, 159, 64, 0.2)",
-//             ],
-//             borderColor: [
-//               "rgba(255, 99, 132, 1)",
-//               "rgba(54, 162, 235, 1)",
-//               "rgba(255, 206, 86, 1)",
-//               "rgba(75, 192, 192, 1)",
-//               "rgba(153, 102, 255, 1)",
-//               "rgba(255, 159, 64, 1)",
-//             ],
-//             borderWidth: 1,
-//           },
-//         ],
-//       },
-//       options: {
-//         scales: {
-//           y: {
-//             beginAtZero: true,
-//           },
-//         },
-//       },
+    // Create the data array for the plot
+    var data = [trace1];
+
+    // Define the plot layout
+    var layout = {
+      title: "Eye Color vs Flicker",
+      xaxis: { title: "Eye Color" },
+      yaxis: { title: "Flicker Frequency" }
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("titlesByDirector", data, layout);
+  })
+};
+
+function renderRating(year, type) {
+  d3.json(`/rating/${year}/${type}`).then(data2 => {
+    var trace1 = {
+      y: data2.filter(thing => thing.type = "TV Show").map(row => row.rating),
+      x: data2.filter(thing => thing.type = "TV Show").map(row => row.title).reverse(),
+      type: "bar",
+      orientation: "h"
+
+    };
+    var trace2 = {
+      y: data2.filter(thing => thing.type = "Movies").map(row => row.rating),
+      x: data2.filter(thing => thing.type = "Movies").map(row => row.title).reverse(),
+      type: "bar",
+      orientation: "h"
+
+    };
+    // Create the Trace
+
+
+    // Create the data array for the plot
+    var data = [trace1, trace2];
+
+    // Define the plot layout
+    var layout = {
+      title: "Eye Color vs Flicker",
+      xaxis: { title: "Eye Color" },
+      yaxis: { title: "Flicker Frequency" },
+      barmode: 'stack'
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("titlesByRating", data, layout);
+  })
+};
+
+//     /*    ========Build a Pie Chart======== */
+//     var data = [{
+//       values: sample_values.slice(0, 10),
+//       labels: otu_ids.slice(0, 10),
+//       hovertext: otu_labels.slice(0, 10),
+//       "type": "pie"
+//     }]
+
+//     var layout = {
+//       title: "Top 10 bacterial species",
+//       showlegend: true,
+//       hoverdistance: 10,
+//       colorway: colorlist
+//     };
+
+//     Plotly.newPlot("moviesVsTV", data, layout, { responsive: true });
+//     console.log("hi");
+/*  ==============END PIE ============== */
+function renderType(year, type) {
+  d3.json(`/type/${year}/${type}`).then(data2 => {
+    var trace1 = {
+      values: data2.map(row => row.title),
+      labels: data2.map(row => row.type),
+      type: "pie"
+
+    };
+    // Create the Trace
+
+
+    // Create the data array for the plot
+    var data = [trace1];
+
+    // Define the plot layout
+    var layout = {
+      title: "Top 10 bacterial species",
+
+      showlegend: true
+
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("moviesVsTV", data, layout);
+  })
+};
+
+
+function renderYear(year, type) {
+  d3.json(`/year/${year}/${type}`).then(data2 => {
+    var trace1 = {
+      x: data2.map(row => row.release_year),
+      y: data2.map(row => row.title),
+      type: "bar"
+
+    };
+    // Create the Trace
+
+
+    // Create the data array for the plot
+    var data = [trace1];
+
+    // Define the plot layout
+    var layout = {
+      title: "Eye Color vs Flicker",
+      xaxis: { title: "Eye Color" },
+      yaxis: { title: "Flicker Frequency" }
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("titlesByYearReleased", data, layout);
+  })
+};
+//   var trace2 = {
+//     x: data2.rating,
+//     y: data2.title,
+//     type: "bar",
+//     transforms: [{
+//       type: "aggregate",
+//       groups: data2.title,
+//       aggregations: [{
+//         target: "y",
+//         function: "avg",
+//         enabled: true
+//       }]
+//     }]
+//   };
+//   // Create the Trace
+
+
+//   // Create the data array for the plot
+//   var data = [trace2];
+
+//   // Define the plot layout
+//   var layout = {
+//     title: "Eye Color vs Flicker",
+//     xaxis: { title: "Eye Color" },
+//     yaxis: { title: "Flicker Frequency" }
+//   };
+
+//   // Plot the chart to a div tag with id "bar-plot"
+//   Plotly.newPlot("titlesByYearReleased", data, layout);
+
+//   console.log(data2);
+// })
+
+
+// function buildCharts(show_id) {
+
+//   var colorlist = ['#56dd3b', '#3ad8bb', '#3ba5e2', '#3956db', '#6639db',
+//     '#9f38e0', '#da36e2', '#93226d', '#701339', '#510811', '#443536', '#1c1919']
+//   // Fetch the sample data for the plots
+//   var url = `/samples/${show_id}`;
+//   d3.json(url).then(function (response) {
+//     const otu_ids = response.otu_ids;
+//     const otu_labels = response.otu_labels;
+//     const sample_values = response.sample_values;
+
+
+
+// function thingy(release_year) {
+//   var url = `/inventory/${release_year}`;
+//   d3.json(url).then(function (response) {
+//     const show_id = response.show_id;
+//     const type = response.type;
+//     const title = response.title;
+//     const release_year = response.release_year;
+
+//     /*    ========Build a Pie Chart======== */
+//     var data = [{
+//       values: release_year.slice(0, 10),
+//       labels: show_id.slice(0, 10),
+//       hovertext: release_year.slice(0, 10),
+//       "type": "pie"
+//     }]
+
+//     var layout = {
+//       title: "Top 10 bacterial species",
+//       showlegend: true,
+//       hoverdistance: 10,
+//       colorway: colorlist
+//     };
+
+//     Plotly.newPlot("moviesVsTV", data, layout, { responsive: true });
+//     /*  ==============END PIE ============== */
+//   });
+// }
+
+
+
+// var renderTitlesByDirector = (years) => $.get(`/inventory/${years}`, (res) => {
+//   var year = Object.keys(res);
+//   var metricValue = Object.values(res);
+//   var data = [
+//     {
+//       x: year,
+//       y: metricValue,
+//       type: 'bar',
+//       marker: {
+//         color: '#212529',
+//         width: 1
+//       }
+//     }
+//   ];
+//   var layout = {
+//     title: `${metric} Time Series`,
+//     yaxis: {
+//       title: `${metric} ($)`
+//     },
+//     xaxis: {
+//       title: `Year`,
+//       tickformat: 'd'
+//     }
+//   };
+
+//   Plotly.newPlot('titlesByDirector', data, layout);
+// })
+
+
+
+// var renderTitlesByDirector = (metric, years) => $.get(`/api/titlesByDirector/${metric}/${years}`, (res) => {
+//   var year = Object.keys(res);
+//   var metricValue = Object.values(res);
+//   var data = [
+//     {
+//       x: year,
+//       y: metricValue,
+//       type: 'bar',
+//       marker: {
+//         color: '#212529',
+//         width: 1
+//       }
+//     }
+//   ];
+//   var layout = {
+//     title: `${metric} Time Series`,
+//     yaxis: {
+//       title: `${metric} ($)`
+//     },
+//     xaxis: {
+//       title: `Year`,
+//       tickformat: 'd'
+//     }
+//   };
+
+//   Plotly.newPlot('titlesByDirector', data, layout);
+// })
+
+// function init() {
+//   // Grab a reference to the dropdown select element
+//   var selector = d3.select("#yearSelector");
+
+//   // Use the list of sample names to populate the select options
+//   d3.json("/inventory/").then((sampleNames) => {
+//     sampleNames.forEach((sample) => {
+//       selector
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
 //     });
-//   </script>
-// </div>
-// {% endblock %}
+
+//     // Use the first sample from the list to build the initial plots
+//     const firstSample = sampleNames[0];
+//     thingy(firstSample);
+//     buildMetadata(firstSample);
+//   });
+// }
+
+// $("#yearSelector").change(() => {
+//   var years = $("#yearSelector").val();
+//   var metric = $("#metricSelector").val();
+//   renderTimeseries(metric, years);
+//   renderCountry(metric, years);
+//   renderChannel(metric, years);
+//   renderProduct(metric, years);
+// });
+
+d3.select(window).on("load", () => {
+  var year = d3.select("#yearSelector").node().value
+  var type = d3.select("#metricSelector").node().value
+  renderDirector(year, type)
+  renderRating(year, type)
+  renderType(year, type)
+  renderYear(year, type)
+});
+
+d3.select("#yearSelector").on("change", () => {
+  var year = d3.select("#yearSelector").node().value
+  var type = d3.select("#metricSelector").node().value
+  renderDirector(year, type)
+  renderRating(year, type)
+  renderType(year, type)
+});
+
+
+
+d3.select("#metricSelector").on("change", () => {
+  var year = d3.select("#yearSelector").node().value
+  var type = d3.select("#metricSelector").node().value
+  renderDirector(year, type)
+  renderRating(year, type)
+  renderYear(year, type)
+});
